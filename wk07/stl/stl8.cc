fca4762229cf1356c2eb25675ab4293ac875ad13
Minttu Niiranen <minttu.m.niiranen@gmail.com>
2022-10-17 18:57:18 +0300

    Stl8

--- wk07/stl/stl8.cc
+++ wk07/stl/stl8.cc
@@ -11,8 +11,12 @@ using namespace std;
  * @param limit a threshold whose lower values are removed
  * @return int EXIT_SUCCESS if everything went OK, EXIT_FAILURE otherwise
  */
+
+
 int removeLessThan(std::vector<int>& v, int limit)
 {
-  return EXIT_FAILURE;
+    remove_if(v.begin(), v.end(), [limit](int i) {return i < limit;});
+
+    return EXIT_SUCCESS;
 }
 
