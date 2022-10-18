19f7132c9760d796ab9ae50eaf1fa54b863614ed
Minttu Niiranen <minttu.m.niiranen@gmail.com>
2022-10-18 14:12:25 +0300

    Stl8

--- wk07/stl/stl8.cc
+++ wk07/stl/stl8.cc
@@ -15,7 +15,7 @@ using namespace std;
 
 int removeLessThan(std::vector<int>& v, int limit)
 {
-    remove_if(v.begin(), v.end(), [limit](int i) {return i < limit;});
+    remove_if(v.begin(), v.end(), [limit](int& i) {return i < limit;});
 
     return EXIT_SUCCESS;
 }
