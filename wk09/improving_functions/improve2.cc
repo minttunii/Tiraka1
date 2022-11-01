56a0d13aa0176732b5400a7f7763e17997b6a55a
Minttu Niiranen <minttu.m.niiranen@gmail.com>
2022-11-01 19:52:10 +0200

    Improve2 toteutettu

--- wk09/improving_functions/improve2.cc
+++ wk09/improving_functions/improve2.cc
@@ -10,11 +10,10 @@
  * @return int - the smallest value or 0 (zero) if vector is empty.
  */
 int minValue(std::vector<int> vec){
-    if ( vec.empty() ) { return 0; }
-    if ( std::is_sorted(vec.begin(), vec.end()) ) {
-        return vec[0];
-    } else {
-        std::sort(vec.begin(), vec.end());
-        return vec[0];
+    if ( vec.empty() ){
+        return 0;
+    }
+    else {
+        return *std::min_element(vec.begin(), vec.end());
     }
 }
