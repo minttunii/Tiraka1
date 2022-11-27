c9c4ebc4f4d7f8cb809cd71c0d029d740688f621
Minttu Niiranen <minttu.m.niiranen@gmail.com>
2022-11-27 13:09:31 +0200

    Muutos maze1 tehtävään

--- wk12/maze/maze1.cc
+++ wk12/maze/maze1.cc
@@ -8,7 +8,7 @@ using namespace std;
 
 // Remember to implement your own container here or in another cc file to store the nodes in the maze
 // If you define it elsewhere, remember to make sure it's accessible by this file as well.
-std::map<std::pair<int, int>, Node*> maze_nodes;
+std::map<std::pair<int, int>, Node> maze_nodes;
 
 /**
  * @brief Create a Node object
@@ -20,14 +20,14 @@ Node* createNode  (std::pair<int, int> loc, std::map<std::string, Node*> paths) 
     auto it = maze_nodes.find(loc);
 
     if(it != maze_nodes.end()){
-        auto node_ptr = it->second;
+        auto node_ptr = &(it->second);
         return node_ptr;
     }
 
     Node newNode;
     newNode.coords = loc;
     newNode.paths = paths;
-    auto it2 = maze_nodes.insert({loc, &newNode});
+    auto it2 = maze_nodes.insert({loc, newNode});
 
-    return it2.first->second;
+    return &(it2.first->second);
 }
